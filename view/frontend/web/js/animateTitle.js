160873d3da75c05aaf47ad383957dbc04fc68a02
Evgeniy Zverev <is3kv3@ukr.net>
2024-02-29 00:36:41 +0200

    Fix incorrect header display on PDP, fixes #1

--- view/frontend/web/js/animateTitle.js
+++ view/frontend/web/js/animateTitle.js
@@ -1,23 +1,32 @@
 require([
     'jquery',
     'Magento_Catalog/js/catalog-add-to-cart'
-], function ($, catalogAddToCart) {
+], function ($) {
     $(document).ready(function () {
         $('body').on('click', '.action.tocart.primary', function () {
-            var originalTitle = document.title;
-            var productName = $(this).closest('.product-item-details').find('.product-item-name').text().trim();
-            var newTitle = 'You added ' + productName + ' to your shopping cart ';
+
+            let originalTitle = document.title;
+            let productName = '';
+            let page_title = $('.product-info-main .page-title-wrapper .page-title');
+            let product_item_link = $(this).closest('.product-item-details').find('.product-item-link');
+
+            if (page_title.length) {
+                productName = page_title.text().trim();
+            } else if (product_item_link.length) {
+                productName = product_item_link.text().trim();
+            }
+            let newTitle = 'You added ' + productName + ' to your shopping cart ';
 
             document.title = newTitle;
 
             setTimeout(function () {
-                var index = 0;
-                var animationInterval = setInterval(function () {
-                    document.title = newTitle.substr(index) + newTitle.substr(0, index);
+
+                let index = 0;
+                let animationInterval = setInterval(function () {
+                    document.title = newTitle.slice(index) + newTitle.slice(0, index);
                     index = (index + 1) % newTitle.length;
                 }, 100);
 
-                // Зупиняємо анімацію після 2 секунд
                 setTimeout(function () {
                     clearInterval(animationInterval);
                     document.title = originalTitle;
